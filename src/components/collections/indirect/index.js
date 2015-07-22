0bd18a306fd4195d5fc7341c8ab461762f829fab
Steve Barnett <steve@naga.co.za>
2015-07-22 10:47:15 +0200

    Updated indirect wording

--- src/components/collections/indirect/index.js
+++ src/components/collections/indirect/index.js
@@ -4,6 +4,6 @@ var Base = require('../base');
 module.exports = Base.extend({
   template: require('./template.html'),
   previewEvent: function() {
-    return "When reached via a different collection";
+    return "When starting this collection";
   }
 });
