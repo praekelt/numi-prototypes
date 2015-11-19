7cc48709c2b097ff2ab90be58577689cbb7347cd
justinvdm <justinvdm.0.1.0@gmail.com>
2015-11-19 02:35:52 +0200

    Better answer saving for choice blocks

--- src/components/blocks/askchoice/index.js
+++ src/components/blocks/askchoice/index.js
@@ -4,6 +4,9 @@ var Base = require('../base');
 
 var AskChoice = Base.extend({
   template: require('./preview.html'),
+  onrender: function() {
+    this.edit();
+  },
   data: function() {
     return {
       text: '',
@@ -29,32 +32,19 @@ AskChoice.Edit = Base.Edit.extend({
   addChoice() {
     this.push('allChoices', newChoice());
   },
-  setChoice(i, val) {
-    this.set('allChoices.' + i + '.text', val);
-  },
-  onChoiceKeyDown(e, i) {
-    if (i < this.get('allChoices').length - 1)
-      this.setChoice(i, denum(i, $(e.original.target).val()));
-    else
-      this.addChoice();
+  onChoiceKeyDown(i) {
+    if (i < this.get('allChoices').length - 1) return;
+    this.addChoice();
   },
   computed: {
     choices: function() {
       return this.get('allChoices').slice(0, -1);
-    },
-    choiceColSpan: function() {
-      return 3 - this.get('useAnswerSaving');
     },
     useAnswerSaving: function() {
       return !!this.get('saveAs');
     }
   }
 });
-
-
-function denum(i, str) {
-  return str.slice(('' + i).length + '. '.length);
-}
 
 
 function newChoice() {
