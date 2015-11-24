e8d6aeefe57c76dbb532d38765b13cdb2b19db38
justinvdm <justinvdm.0.1.0@gmail.com>
2015-11-24 12:49:18 +0200

    Insert user field for freetext

--- src/components/blocks/ask/index.js
+++ src/components/blocks/ask/index.js
@@ -1,4 +1,6 @@
 var Base = require('../base');
+var drawers = require('../../../drawers');
+var Chooser = require('../../../views/chooser');
 
 
 var Ask = Base.extend({
@@ -14,10 +16,30 @@ var Ask = Base.extend({
 
 Ask.Edit = Base.Edit.extend({
   template: require('./edit.html'),
+  oncomplete: function() {
+  },
   computed: {
     useAnswerSaving: function() {
       return !!this.get('saveAs');
     }
+  },
+  insertUserField: function() {
+    var self = this;
+
+    var list = Chooser({
+      el: $('<div>'),
+      data: {
+        title: 'Choose a user field',
+        items: dashboard.get('userFields')
+      }
+    });
+
+    list.once('chosen', function(id, d) {
+      self.set('text', self.get('text') + ' [' + d.name + ']');
+      drawers.close(list);
+    });
+
+    drawers.open(list);
   }
 });
 
--- src/views/chooser/index.js
+++ src/views/chooser/index.js
@@ -1,12 +1,13 @@
+var _ = require('lodash');
 var drawers = require('../../drawers');
 var Ractive = require('ractive');
 
 
 module.exports = Ractive.extend({
   template: require('./template.html'),
-  choose: function(e, id) {
+  choose: function(e, id, d) {
     e.original.preventDefault();
-    this.fire('chosen', id);
+    this.fire('chosen', id, _.find(this.get('items'), {id: id}));
   },
   close: function() {
     drawers.close(this);
