c3fed8aafc8bf4e68e646dfed5690002ec3114ec
justinvdm <justinvdm.0.1.0@gmail.com>
2016-01-06 09:20:49 +0200

    Manage language view ux improvements

--- src/views/manage-languages/index.js
+++ src/views/manage-languages/index.js
@@ -11,9 +11,16 @@ module.exports = Ractive.extend({
   addLanguage: function() {
     dashboard.addLanguage('');
     this.updateLanguages();
+    this.focusNewLanguage();
   },
   data: function() {
     return {languages: dashboard.get('languages')};
+  },
+  focusNewLanguage: function() {
+    $(this.el)
+      .find('.nm-lang-name')
+      .last()
+      .focus();
   },
   updateLanguages: function() {
     // TODO figure out why computed language that returns dashboard's languages
