d9142e3c74a01742bc737d9e1c0534795e4ef803
justinvdm <justinvdm.0.1.0@gmail.com>
2016-01-15 15:02:23 +0200

    Support end states in go2numi

--- utils/go2numi/go2numi.test.js
+++ utils/go2numi/go2numi.test.js
@@ -1,11 +1,36 @@
+require('ractive-require-templates');
+var parse = require('./');
 var chai = require('chai');
 chai.use(require('chai-shallow-deep-equal'));
 chai.should();
 
 
 describe("go2numi", function() {
+  it("should parse end states", function() {
+    parse({
+       start_state: {uuid: 'state1'},
+       states: [{
+         uuid: 'state1',
+         name: 'Ending 1',
+         type: 'end',
+         store_as: 'ending-1',
+         user_defined_store_as: false,
+         entry_endpoint: {uuid: 'endpoint5'},
+         text: 'Thank you for taking our survey',
+       }],
+       connections: []
+    }).should.shallowDeepEqual({
+      sequences: [{
+        blocks: [{
+          id: 'state1',
+          type: 'end',
+          text: 'Thank you for taking our survey',
+        }]
+      }]
+    });
+  });
+
   it("should parse freetext states");
-  it("should parse end states");
   it("should parse unsupported states as annotations");
   it("should parse choice states");
   it("should parse choice states with a more option");
--- utils/go2numi/index.js
+++ utils/go2numi/index.js
@@ -1,5 +1,55 @@
-function parse(model, opts) {
+var _ = require('lodash');
+var uuid = require('node-uuid');
+var blockTypes = require('../../src/views/blocks');
+var Dialogue = require('../../src/views/dialogue');
+var Sequence = require('../../src/views/sequence');
+
+
+function parse(model) {
+  var dialogue = create(Dialogue, {
+    sequences: [create(Sequence)]
+  });
+
+  var d = {
+    dialogue: dialogue,
+    seq: dialogue.sequences[0]
+  };
+
+  model.states.forEach(function(state) {
+    parseState(d, state, model);
+  });
+
+  return d.dialogue;
 }
 
 
+function parseState(d, state, model) {
+  ({
+    end: parseEnd
+  }[state.type] || parseUnsupported)(d, state, model);
+}
+
+
+function parseEnd(d, state, model) {
+  d.seq.blocks.push(create(blockTypes.end, {
+    type: 'end',
+    id: state.uuid,
+    text: state.text
+  }));
+}
+
+
+function parseUnsupported(d, state, model) {
+}
+
+
+function create(type, d) {
+  return _.extend({}, type.prototype.data(), {id: uuid.v4()}, d || {});
+}
+
+
+var parsers = {};
+parsers.parseEnd = parseEnd;
+
 module.exports = parse;
+exports.parsers = parsers;
