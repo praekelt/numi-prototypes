ccfb1bb902ef1722f797c1c44762959799b016d5
justinvdm <justinvdm.0.1.0@gmail.com>
2016-01-19 23:58:13 +0200

    Support choice state in go2numi

--- utils/go2numi/go2numi.test.js
+++ utils/go2numi/go2numi.test.js
@@ -84,22 +84,40 @@ describe("go2numi", function() {
          text: 'Say something'
        }, {
          uuid: 'state2',
-         type: 'freetext',
+         type: 'choice',
+         text: 'What is your favourite colour?',
          entry_endpoint: {uuid: 'endpoint3'},
-         exit_endpoint: {uuid: 'endpoint4'},
-         text: 'Say something else'
+         choice_endpoints: [{
+           uuid: 'endpoint4',
+           label: 'Red'
+         }, {
+           uuid: 'endpoint5',
+           label: 'Blue'
+         }]
        }, {
          uuid: 'state3',
+         type: 'freetext',
+         text: 'Say something else',
+         entry_endpoint: {uuid: 'endpoint6'},
+         exit_endpoint: {uuid: 'endpoint7'}
+       }, {
+         uuid: 'state4',
          type: 'end',
          text: 'Bye',
-         entry_endpoint: {uuid: 'endpoint5'},
+         entry_endpoint: {uuid: 'endpoint8'}
        }],
        connections: [{
          source: {uuid: 'endpoint2'},
          target: {uuid: 'endpoint3'}
        }, {
          source: {uuid: 'endpoint4'},
-         target: {uuid: 'endpoint5'}
+         target: {uuid: 'endpoint6'}
+       }, {
+         source: {uuid: 'endpoint5'},
+         target: {uuid: 'endpoint6'}
+       }, {
+         source: {uuid: 'endpoint7'},
+         target: {uuid: 'endpoint8'}
        }]
     }).should.shallowDeepEqual({
       sequences: [{
@@ -107,7 +125,8 @@ describe("go2numi", function() {
         blocks: [
           {id: 'state1'},
           {id: 'state2'},
-          {id: 'state3'}
+          {id: 'state3'},
+          {id: 'state4'}
         ]
       }]
     });
@@ -120,70 +139,256 @@ describe("go2numi", function() {
        states: [{
          name: 'Start',
          uuid: 'state1',
-         type: 'freetext',
+         type: 'choice',
+         text: 'What is your favourite colour?',
          entry_endpoint: {uuid: 'endpoint1'},
-         exit_endpoint: {uuid: 'endpoint2'},
-         text: 'Say something'
+         choice_endpoints: [{
+           uuid: 'endpoint2',
+           label: 'Red'
+         }, {
+           uuid: 'endpoint3',
+           label: 'Blue'
+         }]
        }, {
-         name: 'Not Shown',
+         name: 'Ask the user to say something',
          uuid: 'state2',
          type: 'freetext',
-         entry_endpoint: {uuid: 'endpoint3'},
-         exit_endpoint: {uuid: 'endpoint4'},
-         text: 'Not actually shown'
+         entry_endpoint: {uuid: 'endpoint4'},
+         exit_endpoint: {uuid: 'endpoint5'},
+         text: 'Say something'
        }, {
-         name: 'Continue',
+         name: 'Ask the user their name',
          uuid: 'state3',
          type: 'freetext',
-         entry_endpoint: {uuid: 'endpoint5'},
-         exit_endpoint: {uuid: 'endpoint6'},
+         entry_endpoint: {uuid: 'endpoint6'},
+         exit_endpoint: {uuid: 'endpoint7'},
+         text: 'What is your name?'
+       }, {
+         name: 'Continue',
+         uuid: 'state4',
+         type: 'freetext',
+         entry_endpoint: {uuid: 'endpoint8'},
+         exit_endpoint: {uuid: 'endpoint9'},
          text: 'Say something else'
        }, {
-         uuid: 'state4',
+         uuid: 'state5',
          type: 'end',
          text: 'Bye',
-         entry_endpoint: {uuid: 'endpoint7'},
+         entry_endpoint: {uuid: 'endpoint10'},
        }],
        connections: [{
          source: {uuid: 'endpoint2'},
-         target: {uuid: 'endpoint5'}
+         target: {uuid: 'endpoint4'}
        }, {
-         source: {uuid: 'endpoint4'},
-         target: {uuid: 'endpoint5'}
+         source: {uuid: 'endpoint3'},
+         target: {uuid: 'endpoint6'}
        }, {
-         source: {uuid: 'endpoint6'},
-         target: {uuid: 'endpoint7'}
+         source: {uuid: 'endpoint5'},
+         target: {uuid: 'endpoint8'}
+       }, {
+         source: {uuid: 'endpoint7'},
+         target: {uuid: 'endpoint8'}
+       }, {
+         source: {uuid: 'endpoint9'},
+         target: {uuid: 'endpoint10'}
        }]
     }).should.shallowDeepEqual({
       sequences: [{
         name: 'Start',
         blocks: [{
-          id: 'state1'
-        }, {
-          type: 'route',
-          seqId: 'seq:state3'
+          id: 'state1',
+          allChoices: [{
+            route: 'seq:state2',
+            text: 'Red'
+          }, {
+            route: 'seq:state3',
+            text: 'Blue'
+          }]
         }]
       }, {
-        id: 'seq:state3',
-        name: 'Continue',
-        blocks: [{
-          id: 'state3'
-        }, {
-          id: 'state4'
-        }]
-      }, {
-        name: 'Not Shown',
+        id: 'seq:state2',
+        name: 'Ask the user to say something',
         blocks: [{
           id: 'state2'
         }, {
           type: 'route',
-          seqId: 'seq:state3'
+          seqId: 'seq:state4'
+        }]
+      }, {
+        id: 'seq:state3',
+        name: 'Ask the user their name',
+        blocks: [{
+          id: 'state3'
+        }, {
+          type: 'route',
+          seqId: 'seq:state4'
+        }]
+      }, {
+        id: 'seq:state4',
+        name: 'Continue',
+        blocks: [{
+          id: 'state4'
+        }, {
+          id: 'state5'
         }]
       }]
     });
   });
 
-  it("should parse choice states with a more option");
+  it("should parse choice states with a more option", function() {
+    parse({
+       start_state: {uuid: 'state1'},
+       states: [{
+         name: 'Start',
+         uuid: 'state1',
+         type: 'choice',
+         text: 'What is your favourite colour?',
+         entry_endpoint: {uuid: 'endpoint1'},
+         choice_endpoints: [{
+           uuid: 'endpoint2',
+           label: 'Red'
+         }, {
+           uuid: 'endpoint3',
+           label: 'More'
+         }]
+       }, {
+         name: 'Ask the user to say something',
+         uuid: 'state2',
+         type: 'freetext',
+         entry_endpoint: {uuid: 'endpoint4'},
+         exit_endpoint: {uuid: 'endpoint5'},
+         text: 'Say something'
+       }, {
+         name: 'Ask the user their name',
+         uuid: 'state3',
+         type: 'freetext',
+         entry_endpoint: {uuid: 'endpoint6'},
+         exit_endpoint: {uuid: 'endpoint7'},
+         text: 'What is your name?'
+       }, {
+         name: 'Continue',
+         uuid: 'state4',
+         type: 'freetext',
+         entry_endpoint: {uuid: 'endpoint8'},
+         exit_endpoint: {uuid: 'endpoint9'},
+         text: 'Say something else'
+       }, {
+         uuid: 'state5',
+         type: 'end',
+         text: 'Bye',
+         entry_endpoint: {uuid: 'endpoint10'},
+       }],
+       connections: [{
+         source: {uuid: 'endpoint2'},
+         target: {uuid: 'endpoint4'}
+       }, {
+         source: {uuid: 'endpoint3'},
+         target: {uuid: 'endpoint6'}
+       }, {
+         source: {uuid: 'endpoint5'},
+         target: {uuid: 'endpoint8'}
+       }, {
+         source: {uuid: 'endpoint7'},
+         target: {uuid: 'endpoint8'}
+       }, {
+         source: {uuid: 'endpoint9'},
+         target: {uuid: 'endpoint10'}
+       }]
+    }).should.shallowDeepEqual({
+      sequences: [{
+        name: 'Start',
+        blocks: [{
+          id: 'state1',
+          allChoices: [{
+            route: 'seq:state2',
+            text: 'Red'
+          }, {
+            route: null,
+            text: 'More'
+          }]
+        }, {
+          id: 'state3'
+        }, {
+          type: 'route',
+          seqId: 'seq:state4'
+        }]
+      }, {
+        id: 'seq:state2',
+        name: 'Ask the user to say something',
+        blocks: [{
+          id: 'state2'
+        }, {
+          type: 'route',
+          seqId: 'seq:state4'
+        }]
+      }, {
+        id: 'seq:state4',
+        name: 'Continue',
+        blocks: [{
+          id: 'state4'
+        }, {
+          id: 'state5'
+        }]
+      }]
+    });
+  });
 
-  it("should parse choice states with a back option");
+  it("should parse choice states with a back option", function() {
+    parse({
+       start_state: {uuid: 'state1'},
+       states: [{
+         name: 'Start',
+         uuid: 'state1',
+         type: 'freetext',
+         entry_endpoint: {uuid: 'endpoint1'},
+         exit_endpoint: {uuid: 'endpoint2'},
+         text: 'Say something'
+       }, {
+         uuid: 'state2',
+         type: 'choice',
+         text: 'What is your favourite colour?',
+         entry_endpoint: {uuid: 'endpoint3'},
+         choice_endpoints: [{
+           uuid: 'endpoint4',
+           label: 'Red'
+         }, {
+           uuid: 'endpoint5',
+           label: 'Back'
+         }]
+       }, {
+         uuid: 'state3',
+         type: 'end',
+         text: 'Bye',
+         entry_endpoint: {uuid: 'endpoint6'},
+       }],
+       connections: [{
+         source: {uuid: 'endpoint2'},
+         target: {uuid: 'endpoint3'}
+       }, {
+         source: {uuid: 'endpoint4'},
+         target: {uuid: 'endpoint6'}
+       }, {
+         source: {uuid: 'endpoint5'},
+         target: {uuid: 'endpoint1'}
+       }]
+    }).should.shallowDeepEqual({
+      sequences: [{
+        name: 'Start',
+        blocks: [{
+          id: 'state1',
+        }, {
+          id: 'state2',
+          allChoices: [{
+            route: null,
+            text: 'Red'
+          }, {
+            route: null,
+            text: 'Back'
+          }]
+        }, {
+          id: 'state3'
+        }]
+      }]
+    });
+  });
 });
--- utils/go2numi/index.js
+++ utils/go2numi/index.js
@@ -10,9 +10,11 @@ function parse(model) {
 
   var d = {
     dialogue: dialogue,
-    blocks: {},
+    blocks: [],
     model: model
   };
+
+  removeBackConnections(d);
 
   model.states.forEach(function(state) {
     parseState(d, state, model);
@@ -29,7 +31,7 @@ function parse(model) {
 
 
 function addStartBlockToSeq(d) {
-  var block = d.blocks[d.model.start_state.uuid];
+  var block = _.find(d.blocks, {id: d.model.start_state.uuid});
   var seq = seqFromBlock(block);
   addSeq(d, seq);
 }
@@ -39,11 +41,7 @@ function parseState(d, state) {
   var fn = parseState[state.type] || parseState.fallback;
   var block = fn(d, state);
 
-  block._meta = {
-    state: state,
-    inSeq: false
-  };
-
+  block._meta = {state: state};
   addBlock(d, block);
 }
 
@@ -66,6 +64,18 @@ parseState.freetext = function(d, state) {
 };
 
 
+parseState.choice = function(d, state) {
+  return create(blockTypes.askchoice, {
+    type: 'askchoice',
+    id: state.uuid,
+    text: state.text,
+    allChoices: state.choice_endpoints
+      .map(choiceFromEndpoint)
+      .concat(createChoice())
+  });
+};
+
+
 parseState.fallback = function(d, state) {
   return create(blockTypes.annotation, {
     type: 'annotation',
@@ -78,25 +88,52 @@ parseState.fallback = function(d, state) {
 function parseConnection(d, connection) {
   var source = findBlockFromEndpointId(d, connection.source.uuid);
   var target = findBlockFromEndpointId(d, connection.target.uuid);
-  var fn = parseConnection[source._meta.state.type] || parseConnection.fallback;
+
+  var fn = parseConnection[getBlockState(source).type]
+        || parseConnection.fallback;
+
   fn(d, source, target, connection);
 }
 
 
 parseConnection.end = _.noop;
-parseConnection.fallback = parseOneToOneState;
 
 
-function parseOneToOneState(d, source, target) {
+parseConnection.choice = function(d, source, target, connection) {
+  var sourceSeq = ensureBlockInSeq(d, source);
+  var choices = choicesWithTarget(d, source, target);
+  choices = _.reject(choices, choiceHasRoute);
+
+  // all choices have the same target, last allChoices item is a stub
+  if (choices.length === choicesWithConnections(d, source).length) {
+    addBlockToSeq(sourceSeq, target);
+    return;
+  }
+
+  if (choices.some(isMoreChoice)) {
+    addBlockToSeq(sourceSeq, target);
+    return;
+  }
+
+  var targetSeq = ensureBlockInSeq(d, target);
+
+  _(choices)
+    .reject(isNavChoice)
+    .each(function(choice) { choice.route = targetSeq.id; })
+    .value();
+};
+
+
+parseConnection.fallback = function(d, source, target) {
   var sourceSeq = ensureBlockInSeq(d, source);
 
-  if (!isMultiTarget(d, target)) {
-    addBlockToSeq(sourceSeq, target);
-  }
-  else {
+  if (isMultiTarget(d, target)) {
     addGotoToSeq(sourceSeq, ensureBlockInSeq(d, target));
   }
-}
+  else {
+    addBlockToSeq(sourceSeq, target);
+  }
+};
 
 
 function create(type, d) {
@@ -105,7 +142,7 @@ function create(type, d) {
 
 
 function addBlock(d, block) {
-  d.blocks[block.id] = block;
+  d.blocks.push(block);
 }
 
 
@@ -116,7 +153,7 @@ function addSeq(d, seq) {
 
 
 function seqFromBlock(block, name) {
-  name = name || block._meta.state.name;
+  name = name || getBlockState(block).name;
 
   var seq = create(Sequence, {
     id: seqIdFromBlock(block),
@@ -135,13 +172,41 @@ function seqIdFromBlock(block) {
 
 
 function addBlockToSeq(seq, block) {
-  seq.blocks.push(block);
+  if (!_.find(seq.blocks, {id: block.id})) seq.blocks.push(block);
 }
 
 
 function isMultiTarget(d, target) {
-  var uuid = target._meta.state.entry_endpoint.uuid;
-  return _.where(d.model.connections, {target: {uuid: uuid}}).length > 1;
+  return blocksWithTarget(d, target).length > 1;
+}
+
+
+function blocksWithTarget(d, target) {
+  var sourceIds = sourcesWithTarget(d, target);
+
+  return d.blocks
+    .filter(function(block) {
+      var endpointIds = getStateExitEndpointIds(getBlockState(block));
+      return _.intersection(sourceIds, endpointIds).length > 0;
+    });
+}
+
+
+function connectionsWithTarget(d, target) {
+  var targetId = getBlockState(target).entry_endpoint.uuid;
+
+  return d.model.connections
+    .filter(function(connection) {
+      return connection.target.uuid === targetId;
+    });
+}
+
+
+function sourcesWithTarget(d, target) {
+  return connectionsWithTarget(d, target)
+    .map(function(connection) {
+      return connection.source.uuid;
+    });
 }
 
 
@@ -171,28 +236,143 @@ function ensureBlockInSeq(d, block) {
 
 function findBlockFromEndpointId(d, endpointId) {
   return _.find(d.blocks, function(block) {
-    return _.contains(getStateEndpointIds(block._meta.state), endpointId);
+    return _.contains(getStateEndpointIds(getBlockState(block)), endpointId);
   });
 }
 
 
 function getStateEndpointIds(state) {
-  var fn = getStateEndpointIds[state.type] || getStateEndpointIds.fallback;
+  return [state.entry_endpoint.uuid]
+    .concat(getStateExitEndpointIds(state));
+}
+
+
+function getStateExitEndpointIds(state) {
+  return _.pluck(getStateExitEndpoints(state), 'uuid');
+}
+
+
+function getStateExitEndpoints(state) {
+  var fn = getStateExitEndpoints[state.type]
+        || getStateExitEndpoints.fallback;
+
   return fn(state);
 }
 
 
-getStateEndpointIds.end = function(state) {
-  return [state.entry_endpoint.uuid];
+getStateExitEndpoints.end = function(state) {
+  return [];
 };
 
 
-getStateEndpointIds.fallback = function(state) {
-  return [
-    state.entry_endpoint.uuid,
-    state.exit_endpoint.uuid
-  ];
+getStateExitEndpoints.choice = function(state) {
+  return state.choice_endpoints;
 };
+
+
+getStateExitEndpoints.fallback = function(state) {
+  return [state.exit_endpoint];
+};
+
+
+function getBlockState(block) {
+  return block._meta.state;
+}
+
+
+function getChoiceEndpoint(choice) {
+  return choice._meta.endpoint;
+}
+
+
+function choicesWithTarget(d, block, target) {
+  var sourceIds = sourcesWithTarget(d, target);
+
+  return block.allChoices
+    .slice(0, -1)
+    .filter(function(choice) {
+      return _.contains(sourceIds, getChoiceEndpoint(choice).uuid);
+    });
+}
+
+function choicesWithConnections(d, block) {
+  var sourceIds = d.model.connections
+    .map(function(connection) {
+      return connection.source.uuid;
+    });
+
+  return block.allChoices
+    .slice(0, -1)
+    .filter(function(choice) {
+      return _.contains(sourceIds, getChoiceEndpoint(choice).uuid);
+    });
+}
+
+
+function choiceFromEndpoint(endpoint) {
+  return createChoice({
+    text: endpoint.label,
+    _meta: {endpoint: endpoint}
+  });
+}
+
+
+function choiceHasRoute(choice) {
+  return choice.route != null;
+}
+
+
+function isNavChoice(choice) {
+  return isBackChoice(choice)
+      || isMoreChoice(choice);
+}
+
+
+function isBackChoice(choice) {
+  return isBackEndpoint(getChoiceEndpoint(choice));
+}
+
+
+function isMoreChoice(choice) {
+  return isMoreEndpoint(getChoiceEndpoint(choice));
+}
+
+
+function isBackEndpoint(endpoint) {
+  return /^back$/i.test(endpoint.label);
+}
+
+
+function isMoreEndpoint(endpoint) {
+  return /^more$/i.test(endpoint.label);
+}
+
+
+function createChoice(d) {
+  return _.extend(
+    blockTypes.askchoice.prototype.newChoice(),
+    {id: uuid.v4()},
+    d || {});
+}
+
+
+function removeBackConnections(d) {
+  d.model.connections = d.model.connections
+    .filter(function(connection) {
+      return !isBackConnection(d, connection);
+    });
+}
+
+
+function isBackConnection(d, connection) {
+  var uuid = connection.source.uuid;
+
+  return d.model.states
+    .some(function(state) {
+      var endpoint = _.find(getStateExitEndpoints(state), {uuid: uuid});
+      return endpoint && isBackEndpoint(endpoint);
+    });
+}
 
 
 module.exports = parse;
